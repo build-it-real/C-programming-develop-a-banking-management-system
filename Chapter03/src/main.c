2e9faf7bab7530c1619539f742877132f0330739
patan-amrulla <patanhub9@gmail.com>
2025-11-15 16:31:19 +0530

    Chapter 03: added menu

--- Chapter03/src/main.c
+++ Chapter03/src/main.c
@@ -1,7 +1,43 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hello, World!\n");
+    int choice;
+
+    do {
+        printf("=== Banking Management System ===\n");
+
+        printf("1. Register\n");
+
+        printf("2. Login\n");
+
+        printf("3. Exit\n");
+
+        printf("Enter your choice: \n");
+
+        scanf("%d", &choice);
+
+        switch (choice)
+        {
+            case 1:
+                printf("Registration feature - Coming soon!!\n");
+                break;
+
+            case 2:
+                printf("Login feature - Coming soon!!\n");
+                break;
+
+            case 3:
+                printf("Exiting...\n");
+                break;
+
+            default:
+                printf("Invalid choice! Please try again...\n");
+                break;
+        }
+
+    } while (choice != 3);
+
+    printf("Thank yu for using Banking Management System");
 
     return 0;
 }
